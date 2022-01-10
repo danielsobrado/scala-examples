88b1198136a81e4e6aff6850847ca886bf7b3da7
danielsobrado <daniel@danielsobrado.com>
2022-01-10 17:22:47 +0400

    Scala 3 New Types

---
+++ scala3/new-types/src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+package com.jds.scala3.newTypes
+
+@main def hello: Unit = 
+  
+  // 1 - literal types
+  val aNumber = 2
+
+  val four: 4 = 4 // 4 <: Int
+
+  def myNumber(n: Int) = println(n)
+  myNumber(5)
+  myNumber(four)
+
+  def myStrictNumber(n: 4) = println(n)
+  myStrictNumber(four)
+  // myStrictNumber(5) // error at compiler time
+
